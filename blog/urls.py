6788881639cb3c8fa1a068af064a6dfbe6fb9f82
antoniahp <a.herrerapuertas@gmail.com>
2023-02-24 12:27:11 +0100

    blog v.2

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
+
+if settings.DEBUG:
+    # When you are using the dev docker-compose, you need to serve the media files somehow
+    urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
